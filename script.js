e586aed32d4428ab184696e122aca9f5c18a67b1
mrittika-n <165163433+mrittika-n@users.noreply.github.com>
2024-10-12 19:50:47 +0530

    Add files via upload
    final upload

--- script.js
+++ script.js
@@ -22,6 +22,55 @@ const activeButton = function () {
 username.addEventListener("keyup", activeButton);
 password.addEventListener("keyup", activeButton);
 
+const splashScreen = document.querySelector(".splash-screen");
+const leftContent = document.querySelector(".content");
+const rightImage = document.querySelector(".image");
+
+setTimeout(() => {
+  leftContent.classList.remove("hidden");
+  rightImage.classList.remove("hidden");
+}, 5000);
+
+// const rightImage = document.querySelector(".image");
 // setTimeout(() => {
-//   document.querySelector(".inner-container").classList.toggle(".hidden");
-// }, 6000);
+//     rightImage.classList.remove("hidden");
+// }, 3000);
+
+//   document.querySelector(".inner-container").classList.remove("hidden");
+// const rightImage = document.querySelector(".content");
+//   document.querySelector(".inner-container").classList.remove("hidden");
+const showPass = document.getElementById("showPass");
+const showPassBtn = function () {
+  if (password.type === "password") {
+    password.type = "text";
+    showPass.textContent = "Hide";
+  } else {
+    password.type = "password";
+    showPass.textContent = "Show";
+  }
+};
+showPass.addEventListener("click", showPassBtn);
+
+const saveBtn = document.querySelector("#submit");
+const hideBtn = document.querySelector(".hide");
+const loader = document.querySelector(".loader");
+// /*
+const laodAni = function () {
+  loader.style.display = "block";
+
+  saveBtn.value = "";
+  setTimeout(() => {
+    // saveBtn.value = "changes saved";
+    // saveBtn.style = "background: #f1f5f4; color: #333; pointer-events: none";
+  }, 2000);
+};
+
+saveBtn.addEventListener("click", laodAni);
+// */
+// saveBtn.onclick = function () {
+//   loader.classList.remove(".hide");
+//   this.setTimeout(() => {
+//     saveBtn.textContent = "changes saved";
+//     saveBtn.style = "background: #f1f5f4; color: #333; pointer-events: none";
+//   }, 2000);
+// };
